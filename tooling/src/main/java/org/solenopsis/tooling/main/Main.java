1d4df1c2f544ff2036cf5071b228d01c7bd8afc7
Scot P. Floess <sfloess@redhat.com>
2013-11-21 08:20:56 -0500

    Now computing apex coverage (may not be accurate)

--- tooling/src/main/java/org/solenopsis/tooling/main/Main.java
+++ tooling/src/main/java/org/solenopsis/tooling/main/Main.java
@@ -11,6 +11,7 @@ import org.solenopsis.lasius.credentials.Credentials;
 import org.solenopsis.lasius.credentials.impl.PropertiesCredentials;
 import org.solenopsis.lasius.sforce.wsimport.tooling.ApexClass;
 import org.solenopsis.lasius.sforce.wsimport.tooling.ApexCodeCoverage;
+import org.solenopsis.lasius.sforce.wsimport.tooling.ApexCodeCoverageAggregate;
 import org.solenopsis.lasius.sforce.wsimport.tooling.ApexTrigger;
 import org.solenopsis.lasius.sforce.wsimport.tooling.Coverage;
 import org.solenopsis.lasius.sforce.wsimport.tooling.QueryResult;
@@ -27,16 +28,28 @@ import org.solenopsis.lasius.wsimport.util.SalesforceWebServiceUtil;
  *
  */
 public class Main {
-    private static final String SOQL_COVERAGE = "select ApexClassorTriggerId, NumLinesCovered, NumLinesUncovered, Coverage from ApexCodeCoverage";
+    private static final String SOQL_COVERAGE = "select NumLinesCovered, NumLinesUncovered, Coverage from ApexCodeCoverageAggregate where ApexClassOrTriggerId = ";
     private static final String SOQL_CLASSES = "select Id, Name from ApexClass";
     private static final String SOQL_TRIGGERS = "select Id, Name from ApexTrigger";
+
+    private static final String SOQL_NEW_COVERAGE = "select Coverage from Code Coverage where ApexClassOrTrigger = ";
 
     private static Map<String, ApexClass> getApexClasses(final SforceServicePortType port) throws Exception {
         final Map<String, ApexClass> retVal = new HashMap<>();
 
-        final QueryResult queryResult = port.query(SOQL_CLASSES);
-        for(final SObject sobj : queryResult.getRecords()) {
-            retVal.put(sobj.getId(), (ApexClass) sobj);
+        boolean isDone = false;
+        QueryResult queryResult = port.query(SOQL_CLASSES);
+
+        while (!isDone) {
+            for(final SObject sobj : queryResult.getRecords()) {
+                retVal.put(sobj.getId(), (ApexClass) sobj);
+            }
+
+            if (queryResult.isDone()) {
+                isDone = true;
+            } else {
+                queryResult = port.queryMore(queryResult.getQueryLocator());
+            }
         }
 
         return retVal;
@@ -45,52 +58,81 @@ public class Main {
     private static Map<String, ApexTrigger> getApexTriggers(final SforceServicePortType port) throws Exception {
         final Map<String, ApexTrigger> retVal = new HashMap<>();
 
-        final QueryResult queryResult = port.query(SOQL_TRIGGERS);
-        for(final SObject sobj : queryResult.getRecords()) {
-            retVal.put(sobj.getId(), (ApexTrigger) sobj);
+        QueryResult queryResult = port.query(SOQL_TRIGGERS);
+        boolean isDone = false;
+
+        while (!isDone) {
+            for(final SObject sobj : queryResult.getRecords()) {
+                retVal.put(sobj.getId(), (ApexTrigger) sobj);
+            }
+
+            if (queryResult.isDone()) {
+                isDone = true;
+            } else {
+                queryResult = port.queryMore(queryResult.getQueryLocator());
+            }
         }
 
         return retVal;
     }
 
-    private static double computePercentage(final ApexCodeCoverage coverage) {
-        return ((double) coverage.getCoverage().getCoveredLines().size() / (coverage.getCoverage().getCoveredLines().size() + coverage.getCoverage().getUncoveredLines().size())) * 100;
-    }
+//    private static double computePercentage(final ApexCodeCoverageAggregate coverage) {
+//        if (coverage.getCoverage().getUncoveredLines().isEmpty()) {
+//            return 0;
+//        }
+//
+//        return ((double) coverage.getCoverage().getCoveredLines().size() / (coverage.getCoverage().getCoveredLines().size() + coverage.getCoverage().getUncoveredLines().size())) * 100;
+//    }
 
+    private static double computePercentage(final ApexCodeCoverageAggregate coverage) {
+        return ((double) coverage.getNumLinesCovered() / (coverage.getNumLinesCovered() + coverage.getNumLinesUncovered())) * 100;
+    }
     private static void emitTooling(final String wsdlType, Credentials creds) throws Exception {
+
+        System.out.println("--->" + creds.getApiVersion());
+
         final SforceServicePortType port = SalesforceWebServiceUtil.createToolingPort(creds);
 
         final Map<String, ApexClass> classMap = getApexClasses(port);
         final Map<String, ApexTrigger> triggerMap = getApexTriggers(port);
 
-        final Map<String, ApexCodeCoverage> classList = new TreeMap<>();
-        final Map<String, ApexCodeCoverage> triggerList = new TreeMap<>();
+        final TreeMap<String, String> classList = new TreeMap<>();
+        final TreeMap<String, String> triggerList = new TreeMap<>();
 
-        final QueryResult queryResult = port.query(SOQL_COVERAGE);
+        for (final String id : classMap.keySet()) {
+            QueryResult queryResult = port.query(SOQL_COVERAGE + "'" + id + "'");
 
-        for(final SObject sobj : queryResult.getRecords()) {
-            ApexCodeCoverage coverage = (ApexCodeCoverage) sobj;
+            for(final SObject sobj : queryResult.getRecords()) {
+                ApexCodeCoverageAggregate coverage = (ApexCodeCoverageAggregate) sobj;
 
-            if (classMap.containsKey(coverage.getApexClassOrTriggerId())) {
 
-                classList.put(classMap.get(coverage.getApexClassOrTriggerId()).getName(), coverage);
-            } else if (triggerMap.containsKey(coverage.getApexClassOrTriggerId())) {
-                triggerList.put(triggerMap.get(coverage.getApexClassOrTriggerId()).getName(), coverage);
+                classList.put(classMap.get(id).getName(),String.format("%-40s%13.2f", classMap.get(id).getName(), computePercentage(coverage)));
             }
         }
 
-        System.out.println("Triggers:");
+        for (final String id : triggerMap.keySet()) {
+            QueryResult queryResult = port.query(SOQL_COVERAGE + "'" + id + "'");
 
-        for (final String str : triggerList.keySet()) {
-            System.out.printf("    %-40s%13.2f", str, computePercentage(triggerList.get(str)));
-            System.out.println("%");
+            for(final SObject sobj : queryResult.getRecords()) {
+                ApexCodeCoverageAggregate coverage = (ApexCodeCoverageAggregate) sobj;
+
+
+                triggerList.put(triggerMap.get(id).getName(),String.format("%-40s%13.2f", triggerMap.get(id).getName(), computePercentage(coverage)));
+            }
         }
 
-        System.out.println("\nClasses:");
+        System.out.println("\n\n");
+        System.out.println("Class Coverage:");
 
-        for (final String str : classList.keySet()) {
-            System.out.printf("    %-40s%13.2f", str, computePercentage(classList.get(str)));
-            System.out.println("%");
+        for (final String str : classList.values()) {
+            System.out.println("    " + str + "%");
+        }
+
+        System.out.println("\n\n");
+        System.out.println("Trigger Coverage:");
+
+        for (final String str : triggerList.values()) {
+            System.out.println("    " + str + "%");
         }
     }
 
@@ -102,7 +144,5 @@ public class Main {
 
         emitTooling("Partner WSDL", new PropertiesCredentials(new FilePropertiesMgr(System.getProperty("user.home") + "/.solenopsis/credentials/" + env)));
 
-        //emitTooling("Partner WSDL", credentials, new PartnerSecurityMgr());
-        //emitTooling("Enterprise WSDL", credentials, new EnterpriseSecurityMgr());
     }
 }
